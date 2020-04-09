4356bd142714d118bc8dbe0beac498eaa2b94757
Игорь Забавнов <iz@avp.su>
2020-04-09 23:15:15 +0300

    add SJ

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
  openssl genrsa -out ca.key 2048
 
 # Create a self signed Certificate, valid for 10yrs with the 'signing' option set
- openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+ openssl req -x509 -new -nodes -key ca.key -subj "/CN=Zabavnov" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
 
 kubectl create secret tls ca-key-pair \
    --cert=ca.crt \
