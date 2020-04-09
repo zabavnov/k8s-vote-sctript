00ac55a103909c9b40db4b1df8b5c9214346670f
Игорь Забавнов <iz@avp.su>
2020-04-10 01:20:29 +0300

    fix certs

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 
 
 # Create a self signed Certificate, valid for 10yrs with the 'signing' option set
-openssl req -x509 -new -nodes -key ca.key -subj "/CN=Zabavnov" -days 365 -reqexts v3_req -extensions v3_ca -out ca.cr
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=Zabavnov" -days 365 -reqexts v3_req -extensions v3_ca -out ca
 
 openssl genrsa -out vote.key 2048
 
 openssl req -new -sha256 -key vote.key -subj "/C=US/ST=CA/O=Test/CN=vote.s48.su" -out vote.csr
 
-openssl x509 -req -in vote.csr -CA ca.cr -CAkey ca.key -out vote.crt -days 500 -sha256
+openssl x509 -req -in vote.csr -CA ca -CAkey ca.key -out vote.crt -days 500 -sha256
 
 cat vote.crt \
-ca.cr> combined-certificates.crt
+ca> combined-certificates.crt
 
 kubectl delete secret tls-key-pair-combo
 
