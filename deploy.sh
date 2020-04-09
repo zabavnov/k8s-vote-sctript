3ff445d442e398ab747a28cc095907e1c06b71f1
Игорь Забавнов <iz@avp.su>
2020-04-09 23:57:47 +0300

    add csr

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,16 @@
  openssl genrsa -out ca.key 2048
 
 # Create a self signed Certificate, valid for 10yrs with the 'signing' option set
- openssl req -x509 -new -nodes -key ca.key -subj "/CN=Zabavnov" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+ openssl req -x509 -new -nodes -key ca.key -subj "/CN=Zabavnov" -days 365 -reqexts v3_req -extensions v3_ca -out ca.crt
 
-kubectl create secret tls ca-key-pair \
-   --cert=ca.crt \
+openssl req -new -sha256 -key vote.key -subj "/C=US/ST=CA/O=Test/CN=vote.s48.su" -out vote.csr
+
+openssl x509 -req -in vote.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out vote.crt -days 500 -sha256
+
+cat vote.crt \ ca.crt\  > combined-certificates.crt
+
+kubectl create secret tls tls-key-pair \
+   --cert=combined-certificates.crt \
    --key=ca.key \
    --namespace=default
 
