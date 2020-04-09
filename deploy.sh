1978871aefd8cc6e5804270afee27380a4e7d258
Игорь Забавнов <iz@avp.su>
2020-04-10 00:24:05 +0300

    fix "

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 ls
 
 # Create a self signed Certificate, valid for 10yrs with the 'signing' option set
- openssl req -x509 -new -nodes -key ca.key -subj "/CN=Zabavnov" -days 365 -reqexts v3_req -extensions v3_ca -out ca.crt
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=Zabavnov" -days 365 -reqexts v3_req -extensions v3_ca -out ca.crt
 
 cat ca.crt
 
@@ -16,7 +16,7 @@ openssl req -new -sha256 -key vote.key -subj "/C=US/ST=CA/O=Test/CN=vote.s48.su"
 
 openssl x509 -req -in vote.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out vote.crt -days 500 -sha256
 
-cat vote.crt \ ca.crt  > combined-certificates.crt
+cat vote.crt ca.crt  > combined-certificates.crt
 
 kubectl create secret tls tls-key-pair \
    --cert=combined-certificates.crt \
