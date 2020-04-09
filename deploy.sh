9a9042e8a1b26faaa140162b859dde0cb5086456
Игорь Забавнов <iz@avp.su>
2020-04-09 23:08:43 +0300

    add CA generate

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
+# Generate a CA private key
+$ openssl genrsa -out ca.key 2048
+
+# Create a self signed Certificate, valid for 10yrs with the 'signing' option set
+$ openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+
+kubectl create secret tls ca-key-pair \
+   --cert=ca.crt \
+   --key=ca.key \
+   --namespace=default
+
 kubectl apply -f k8s-specifications
